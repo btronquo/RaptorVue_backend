c97e79047849263730b589efb56847ab6eac8380
Btrnq <b.tronquoy@gmail.com>
2018-11-29 17:41:38 +0100

    (fix) setup config.js for use of var env

--- config/config.js
+++ config/config.js
@@ -1,14 +1,22 @@
 require('dotenv').config()
 
 module.exports = {
-  port: process.env.PORT || 8081,
-  db: {
-    database: process.env.DB_NAME || 'raptor',
-    user: process.env.DB_USER || 'raptor',
-    password: process.env.DB_PASS || 'raptor',
-    options: {
-      dialect: process.env.DB_DIALECT || 'mysql',
-      host: process.env.DB_HOST || 'localhost'
-    }
+  'development': {
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASS,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'dialect': process.env.DB_DIALECT
+  },
+  'test': {
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASS,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'dialect': process.env.DB_DIALECT
+  },
+  'production': {
+    'use_env_variable': 'MYSQL_URL',
+    'dialect': 'mysql'
   }
 }
